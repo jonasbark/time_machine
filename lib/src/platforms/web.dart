6960ff890d45ac17b7a6bb39a636a2b0031d4cd2
Jonas Bark <jonas.bark@gmx.de>
2021-03-01 15:27:02 +0100

    allow flutter web execution from a subfolder

--- lib/src/platforms/web.dart
+++ lib/src/platforms/web.dart
@@ -14,89 +14,42 @@ import 'package:time_machine/src/time_machine_internal.dart';
 
 import 'platform_io.dart';
 
-/// Resource package currently uses Isolate.resolvePackageUri (see: https://github.com/dart-lang/resource/issues/35)
-/// A fix is pending, but it is very slow coming (see: https://github.com/dart-lang/resource/pull/36)
-
-@ddcSupportHack
-Uri _resolveUri(Uri uri) {
-  if (uri.scheme == 'package') {
-    uri = Uri.parse('packages/${uri.path}');
-  }
-  return Uri.base.resolveUri(uri);
-}
-
-@ddcSupportHack
-Future<List<int>> _httpGetBytes(Uri uri) {
-  return HttpRequest.request(uri.toString(), responseType: 'arraybuffer')
-      .then((request) {
-    ByteBuffer data = request.response;
-    return data.asUint8List();
-  });
-}
-
-@ddcSupportHack
-/// Reads the bytes of a URI as a list of bytes.
-Future<List<int>> _readAsBytes(Uri uri) async {
-  if (uri.scheme == 'http' || uri.scheme == "https") {
-    return _httpGetBytes(uri);
-  }
-  if (uri.scheme == 'data') {
-    return uri.data.contentAsBytes();
-  }
-  throw UnsupportedError('Unsupported scheme: $uri');
-}
-
-@ddcSupportHack
-/// Reads the bytes of a URI as a string.
-Future<String> _readAsString(Uri uri, Encoding encoding) async {
-  if (uri.scheme == 'http' || uri.scheme == "https") {
-    // Fetch as string if the encoding is expected to be understood,
-    // otherwise fetch as bytes and do decoding using the encoding.
-    if (encoding != null) {
-      return encoding.decode(await _httpGetBytes(uri));
-    }
-    return HttpRequest.getString(uri.toString());
-  }
-  if (uri.scheme == 'data') {
-    return uri.data.contentAsString(encoding: encoding);
-  }
-  throw UnsupportedError('Unsupported scheme: $uri');
-}
-
 class _WebMachineIO implements PlatformIO {
+  final dynamic _rootBundle;
+
+  _WebMachineIO(this._rootBundle);
+
   @override
   Future<ByteData> getBinary(String path, String filename) async {
     if (filename == null) return ByteData(0);
 
-    // var resource = new Resource('packages/time_machine/data/$path/$filename');
-    // // todo: probably a better way to do this
-    // var binary = new ByteData.view(new Int8List.fromList(await resource.readAsBytes()).buffer);
-
-    var resource = Uri.parse('${Uri.base.origin}/packages/time_machine/data/$path/$filename');
-    var binary = ByteData.view(Int8List.fromList(await _readAsBytes(resource)).buffer);
-
-    return binary;
+    ByteData byteData =
+        await _rootBundle.load('packages/time_machine/data/$path/$filename');
+    return byteData;
   }
 
   @override
-  Future/**<Map<String, dynamic>>*/ getJson(String path, String filename) async {
-    // var resource = new Resource('packages/time_machine/data/$path/$filename');
-    // return json.decode(await resource.readAsString());
-
-    var resource = Uri.parse('${Uri.base.origin}/packages/time_machine/data/$path/$filename');
-    return json.decode(await _readAsString(_resolveUri(resource), null));
+  // may return Map<String, dynamic> or List
+  Future getJson(String path, String filename) async {
+    String text = await _rootBundle
+        .loadString('packages/time_machine/data/$path/$filename');
+    return json.decode(text);
   }
 }
 
-Future initialize(Map args) => TimeMachine.initialize();
+Future initialize(Map args) {
+  if (args == null || args['rootBundle'] == null)
+    throw Exception(
+        "Pass in the rootBundle from 'package:flutter/services.dart';");
+  // Map IO functions
+  PlatformIO.local = _WebMachineIO(args['rootBundle']);
+  return TimeMachine.initialize();
+}
 
 class TimeMachine {
   // I'm looking to basically use @internal for protection??? <-- what did I mean by this?
   static Future initialize() async {
     Platform.startWeb();
-
-    // Map IO functions
-    PlatformIO.local = _WebMachineIO();
 
     // Default provider
     var tzdb = await DateTimeZoneProviders.tzdb;
@@ -145,8 +98,7 @@ class TimeMachine {
       _yearFormat = options['year'];
       _monthFormat = options['month'];
       _dayFormat = options['day'];
-    }
-    catch (e, s) {
+    } catch (e, s) {
       print('Failed to get platform local information.\n$e\n$s');
     }
   }
